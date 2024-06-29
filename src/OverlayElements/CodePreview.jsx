e60d8514dd5c01dde454dccca0894095cc28df64
Juan Alvarez <KilloPillers@hotmail.com>
2024-06-28 23:34:46 -0700

    Ported code generation from MapBuilder 1.0

--- src/OverlayElements/CodePreview.jsx
+++ src/OverlayElements/CodePreview.jsx
@@ -3,57 +3,118 @@ import { useEffect } from 'react';
 import Paper from '@mui/material/Paper'; 
 import Button from '@mui/material/Button';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import { MyContext } from '../MyContext';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { MenuTheme } from '../Themes/MenuTheme';
+import CheckIcon from '@mui/icons-material/Check';
+import Link from '@mui/material/Link';
 import './CodePreview.css';
 
-const testCodeString = `local classid = nil;
-local currUnitIndex = 1;
-local unitid = nil;
-local personality = nil;
-local enemyUnit = nil;
-local position = nil;
-classid = 21;
-unitid = 4;
-position = Vector2.new(1, 11);
-
-enemyUnit = Unit.new(position, 0,-1)
-enemyUnit.classid = classid
-enemyUnit.weapon = ClassLookup[classid]["WeaponType"];
-enemyUnit.range = ClassLookup[classid]["Move"];
-enemyUnit.class = ClassLookup[classid]["Name"];
-enemyUnit.rawStats = ClassLookup[classid]["StartingStats"]
-enemyUnit.img = ClassLookup[classid]["Image"];
-enemyUnit.unitIndex = currUnitIndex;
-currUnitIndex+=1;
-for i,v in pairs(ClassLookup[classid]["ElementResists"]) do
-    enemyUnit[i] = v;
-end
-
-enemyUnit.lvl = 3
-enemyUnit.rawStats.maxhp = 50
-enemyUnit.hp = 50;
-enemyUnit.rawStats.maxsp = 20
-enemyUnit.sp = 20;
-enemyUnit.rawStats.atk = 21
-enemyUnit.rawStats.def = 16
-enemyUnit.rawStats.spd = 14
-enemyUnit.rawStats.hit = 15
-enemyUnit.rawStats.int = 10
-enemyUnit.rawStats.res = 10
-enemyUnit.id = 4;
-enemyUnit.name = "Rita"` 
-
 const CodePreview = React.forwardRef((props, ref) => {
+  const { mapData, updateMap } = React.useContext(MyContext);
   const [codeType, setCodeType] = React.useState('Unit');
-  const [codeString, setCodeString] = React.useState(testCodeString);
-  
+  const [codeString, setCodeString] = React.useState('');
+  const [unitCode, setUnitCode] = React.useState('');
+  const [mapCode, setMapCode] = React.useState('');
+  const [copied, setCopied] = React.useState(false);
+
+  useEffect(() => {
+    generateCode();
+    setCodeString(unitCode);
+    setCodeType('Unit');
+  }, []);
+
   useEffect(() => {
     // ensure prism is loade before attempting to highlight'
     if (window.Prism) {
       console.log('highlighting code');
       window.Prism.highlightAll();
     }
-  }, []);
+  }, [codeString]);
+
+  function generateCode() { 
+    let deploy_positions = '';
+    let action_tiles = '';
+    let unit_tiles = [];
+    let map_code = "local newheights = {};\n";
+    map_code += "local mapXSize = " + mapData.width + ";\n";
+    map_code += "local mapYSize = " + mapData.height + ";\n";
+  
+    for (let i = 1; i <= mapData.width; i++) {
+      map_code += "newheights[" + i + "] = {"
+      for (let j = 1; j <= mapData.height; j++) {
+        console.log('mapData.ButtonGrid[i-1][j-1]:', mapData.ButtonGrid[i-1][j-1])
+        if (mapData.ButtonGrid[i-1][j-1].is_deploy_position) {
+          deploy_positions += "table.insert(newmap.deployPositions, Vector2.new(" + j + ", " + i + "));\n";
+        }
+        if (mapData.ButtonGrid[i-1][j-1].is_action_tile) {
+          let tile_name = mapData.ButtonGrid[i-1][j-1].tile_name;
+          let event_id = mapData.ButtonGrid[i-1][j-1].event_id;
+
+          action_tiles += "table.insert(newmap.actiontiles, ActionTile.new(" + tile_name + ", " + event_id + ", Vector2.new(" + j + ", " + i + "));\n";
+        }
+        if (mapData.ButtonGrid[i-1][j-1].has_unit) {
+          let unit_tile = mapData.ButtonGrid[i-1][j-1];
+          unit_tiles.push(unit_tile);
+        }
+        map_code += mapData.ButtonGrid[i-1][j-1].tile_height
+        if (j < mapData.height) {
+          map_code += ", ";
+        }
+      }
+      map_code += "};\n"; 
+    }
+    map_code += "local newmap = GameMap.new(newheights, mapXSize, mapYSize);\n";
+    map_code += deploy_positions;
+    map_code += action_tiles;   
+    
+    // Unit Code Below
+    let unit_code = '';
+    let header = "local classid = nil;\nlocal currUnitIndex = 1;\nlocal unitid = nil;\nlocal personality = nil;\nlocal enemyUnit = nil;\nlocal position = nil;\n"
+    let unit_id_pos = "classid = {};\nunitid = {};\nposition = Vector2.new{};\n\nenemyUnit = Unit.new(position, 0,-1)\n"
+    let tablecode = "enemyUnit.classid = classid\nenemyUnit.weapon = ClassLookup[classid][\"WeaponType\"];\nenemyUnit.range = ClassLookup[classid][\"Move\"];\nenemyUnit.class = ClassLookup[classid][\"Name\"];\nenemyUnit.rawStats = ClassLookup[classid][\"StartingStats\"]\nenemyUnit.img = ClassLookup[classid][\"Image\"];\nenemyUnit.unitIndex = currUnitIndex;\ncurrUnitIndex+=1;\nfor i,v in pairs(ClassLookup[classid][\"ElementResists\"]) do\n    enemyUnit[i] = v;\nend\n\n"
+    let unit_stats = "enemyUnit.lvl = {}\nenemyUnit.rawStats.maxhp = {}\nenemyUnit.hp = {};\nenemyUnit.rawStats.maxsp = {}\nenemyUnit.sp = {};\nenemyUnit.rawStats.atk = {}\nenemyUnit.rawStats.def = {}\nenemyUnit.rawStats.spd = {}\nenemyUnit.rawStats.hit = {}\nenemyUnit.rawStats.int = {}\nenemyUnit.rawStats.res = {}\nenemyUnit.id = {};\nenemyUnit.name = \"{}\"\n"
+    let footer = "personality = \"{}\"\nenemyUnit.personality = game:GetService(\"ServerStorage\").AIPersonalities:FindFirstChild(personality);\nenemyUnit.deathevent = {};\n\nnewmap:PlaceUnit(enemyUnit,position.x,position.y);\n\n\n"
+    unit_code += header;
+
+    for (let i = 0; i < unit_tiles.length; i++) {
+      let unit_tile = unit_tiles[i];
+      console.log('unit_tile:', unit_tiles[i]);
+      let unit = unit_tile.unit;
+      let position = "(" + unit_tile.tile_position[0]+1 + ", " + unit_tile.tile_position[1]+1 + ")";
+      unit_code += unit_id_pos.replace('{}', unit.classId)
+      .replace('{}', unit.unitId)
+      .replace('{}', position); 
+
+      unit_code += tablecode;
+
+      unit_code += unit_stats.replace('{}', unit.unitLevel)
+      .replace('{}', unit.maxHp)
+      .replace('{}', unit.maxHp)
+      .replace('{}', unit.maxSp)
+      .replace('{}', unit.maxSP)
+      .replace('{}', unit.Atk)
+      .replace('{}', unit.Def)
+      .replace('{}', unit.Spd)
+      .replace('{}', unit.Hit)
+      .replace('{}', unit.Int)
+      .replace('{}', unit.Res)
+      .replace('{}', unit.unitID)
+      .replace('{}', unit.unitName);
+      
+      for (let j = 0; j < unit.skills.length; j++) {
+        if (unit.skills[j] !== "") {
+          unit_code += "table.insert(enemyUnit.skills, " + unit.skills[j] + ");\n"; 
+        }
+      }
+        unit_code += "\n";
+        unit_code += footer.replace('{}', unit.personality).replace('{}', unit.deathEvent);
+    }
+    setUnitCode(unit_code);
+    setMapCode(map_code);
+  }
 
   return (
     <ThemeProvider theme={MenuTheme}>
@@ -72,14 +133,62 @@ const CodePreview = React.forwardRef((props, ref) => {
           sx={{margin: '1em'}}
           variant="contained"
           color="secondary"
+          onClick={() => {
+            setCodeString(mapCode);
+            setCodeType('Map');
+            if (window.Prism) {
+              window.Prism.highlightAll();
+            }
+          }}
         >Map</Button>
         <Button
           sx={{margin: '1em'}}
           variant="contained"
           color="secondary"
+          onClick={() => {
+            setCodeString(unitCode);
+            setCodeType('Unit');
+            if (window.Prism) {
+              window.Prism.highlightAll();
+            }
+          }}
         >Unit</Button>
         </Paper>
-        <h3>{codeType} Code Preview</h3>
+        <Paper
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.1)'
+          }}
+        >
+          <Typography variant="caption" gutterBottom> {codeType} Code Preview </Typography>
+          <Tooltip title="Copy code" arrow>
+          <Link
+            sx={{cursor: 'pointer'}}
+            underline="none"
+            onClick={() => {
+              navigator.clipboard.writeText(codeString);
+              setCopied(true); 
+              setTimeout(() => {
+                setCopied(false);
+              }, 2000);
+            }}
+          >
+            {copied ? (
+            <>
+                <CheckIcon sx={{ fontSize: 10 }}/>
+                <Typography variant="caption" gutterBottom> Copied! </Typography>
+            </>
+              ) : (
+                  <>
+                <ContentCopyIcon sx={{ fontSize: 10 }}/>
+                <Typography variant="caption" gutterBottom> Copy code </Typography>
+              </>
+                )}
+            </Link>
+          </Tooltip>
+        </Paper>
         <pre>
           <code className="language-lua">
             {codeString}
